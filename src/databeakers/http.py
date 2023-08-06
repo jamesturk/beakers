25273a0851ba893da475d4627e41904d87d0c717
James Turk <dev@jamesturk.net>
2023-08-05 20:15:00 -0500

    option to follow redirects

--- src/databeakers/http.py
+++ src/databeakers/http.py
@@ -19,18 +19,20 @@ class HttpRequest:
     Filter that converts from a beaker with a URL to a beaker with an HTTP response.
     """
 
-    def __init__(self, field: str = "url"):
+    def __init__(self, field: str = "url", *, follow_redirects: bool = True) -> None:
         """
         Args:
             field: The name of the field in the beaker that contains the URL.
+            follow_redirects: Whether to follow redirects.
         """
         self.field = field
+        self.follow_redirects = follow_redirects
 
     async def __call__(self, item: BaseModel) -> HttpResponse:
         url = getattr(item, self.field)
 
         async with httpx.AsyncClient() as client:
-            response = await client.get(url)
+            response = await client.get(url, follow_redirects=self.follow_redirects)
 
         return HttpResponse(
             url=url,
