e6d5d9a982ef4d3dc7a227958f891134976c4058
James Turk <dev@jamesturk.net>
2023-08-08 17:01:02 -0500

    add simple retries and throttling for http

--- src/databeakers/http.py
+++ src/databeakers/http.py
@@ -1,6 +1,7 @@
 import httpx
 from pydantic import BaseModel, Field
 import datetime
+import asyncio
 
 
 class HttpResponse(BaseModel):
@@ -12,6 +13,20 @@ class HttpResponse(BaseModel):
     status_code: int
     response_body: str
     retrieved_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
+
+
+class SteadyRateLimit:
+    """
+    Filter that limits the rate of items flowing through the pipeline.
+    """
+
+    def __init__(self, f_callable, sleep_seconds: float):
+        self.callable = f_callable
+        self.sleep_seconds = sleep_seconds
+
+    async def __call__(self, item: BaseModel) -> BaseModel:
+        await asyncio.sleep(self.sleep_seconds)
+        return await self.callable(item)
 
 
 class HttpRequest:
@@ -27,12 +42,14 @@ class HttpRequest:
         """
         self.field = field
         self.follow_redirects = follow_redirects
+        transport = httpx.AsyncHTTPTransport(retries=1)
+        self.client = httpx.AsyncClient(transport=transport)
 
     async def __call__(self, item: BaseModel) -> HttpResponse:
         url = getattr(item, self.field)
 
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, follow_redirects=self.follow_redirects)
+        # async with self.client as client:
+        response = await self.client.get(url, follow_redirects=self.follow_redirects)
 
         return HttpResponse(
             url=url,
